b8c535f79808adee2e89c2e73cb0db29c6070bba
Kunjeti Venkata Nanda Kumar <63538807+kunjetivenkatanandakumar@users.noreply.github.com>
2026-06-07 22:04:01 +0530

    Add files via upload

---
+++ Ecommerce-RNN/app.py
@@ -0,0 +1,33 @@
+import streamlit as st
+import numpy as np
+
+from tensorflow.keras.models import load_model
+from sklearn.preprocessing import MinMaxScaler
+
+model = load_model("sales_rnn_model.keras")
+
+st.title("E-Commerce Sales Forecast")
+
+day1 = st.number_input("Day 1 Sales")
+day2 = st.number_input("Day 2 Sales")
+day3 = st.number_input("Day 3 Sales")
+
+if st.button("Predict"):
+
+    scaler = MinMaxScaler()
+
+    sample = np.array([[day1],[day2],[day3]])
+
+    scaler.fit(sample)
+
+    scaled = scaler.transform(sample)
+
+    X = scaled.reshape(1,3,1)
+
+    pred = model.predict(X)
+
+    result = scaler.inverse_transform(pred)
+
+    st.success(
+        f"Predicted Next Day Sales: {result[0][0]:.2f}"
+    )
